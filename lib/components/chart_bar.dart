09037701bac81c2b513b0dd91fd2b1c36bb18ecb
Mateus Medeiros <mateus.med144@gmail.com>
2022-02-08 10:08:35 -0300

    Criando gráfico de gastos diários dos últimos 7 dias

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  const ChartBar({
+    required this.label,
+    required this.value,
+    required this.percentage,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FittedBox(
+          child: Text(value.toStringAsFixed(2)),
+        ),
+        SizedBox(height: 5),
+        Container(
+          height: 80,
+          width: 15,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 2),
+                    color: const Color.fromRGBO(222, 222, 222, 1),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label)
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
 
@@ -21,12 +22,10 @@ class ExpenseApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         fontFamily: 'Quicksand',
         textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 19,
-            fontWeight: FontWeight.bold
-          )
-        ),
+            headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 19,
+                fontWeight: FontWeight.bold)),
       ),
     );
   }
@@ -39,42 +38,81 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //     id: "t1",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t2",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t3",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t4",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t5",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t6",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
-    // Transaction(
-    //     id: "t7",
-    //     title: "Conta de Energia",
-    //     value: 195.89,
-    //     date: DateTime.now()),
+    Transaction(
+      id: "t1",
+      title: "Conta de Energia",
+      value: 80.5,
+      date: DateTime.now().subtract(Duration(days: 1)),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de água",
+      value: 20.5,
+      date: DateTime.now().subtract(Duration(days: 2)),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Conta de Internet",
+      value: 30.5,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Conta de Internet",
+      value: 70.5,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Conta de Internet",
+      value: 30.5,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Conta de Internet",
+      value: 80.5,
+      date: DateTime.now().subtract(Duration(days: 2)),
+    ),
+    Transaction(
+      id: "t4",
+      title: "Restaurante",
+      value: 40.5,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
+    Transaction(
+      id: "t5",
+      title: "TV a cabo",
+      value: 50.5,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t6",
+      title: "IPTU",
+      value: 60.5,
+      date: DateTime.now().subtract(Duration(days: 5)),
+    ),
+    Transaction(
+      id: "t7",
+      title: "IPVA",
+      value: 70.5,
+      date: DateTime.now().subtract(Duration(days: 6)),
+    ),
   ];
+
+  ///Retorna a lista de transações dos últimos 7 dias
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((transaction) {
+      //Verifica se a data da transação está depois de (dia atual - 7)
+      //Será se isso está certo mesmo?
+      //Como está verificando se a data está depois dos últimos 7 dias
+      //Se a pessoa digitar uma data futura ex: 31/03/2050
+      //Pelo que entendi vai passar na condição e será adicionada na lista de transações recentes
+      return transaction.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
 
   _addTransaction(String title, double value) {
     final newTransaction = Transaction(
@@ -115,10 +153,9 @@ class _HomePageState extends State<HomePage> {
         title: const Text(
           'Despesas Pessoais',
           style: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
         ),
         actions: <Widget>[
           IconButton(
@@ -133,14 +170,7 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             //Gráfico de despesas por dia na semana atual
-            Container(
-              width: double.infinity,
-              child: const Card(
-                color: Colors.blue,
-                child: Text("Gráfico de Gastos"),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions),
           ],
         ),
